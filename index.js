26d6a0febc2add06e9169af7ddeca4bc1d38de07
Martynas-J <Aurimhas@gmail.com>
2023-05-25 20:42:10 +0300

    home page done v1.0

--- index.js
+++ index.js
@@ -7,34 +7,65 @@ document.body.prepend(nav())
 home()
 async function home() {
     let home = document.querySelector("#home")
-    const data = await fetchData(`${API_URL}/posts?_expand=user`)
-    let homeDiv = createHomeData(data.slice(-5))
-    home.append(homeDiv)
+
+    const postsData = await fetchData(`${API_URL}/posts?_expand=user`)
+    let posts = createHomeData(postsData.slice(-5), "posts")
+
+    const albumsData = await fetchData(`${API_URL}/albums?_embed=photos&_expand=user`)
+    let albums = createHomeData(albumsData.slice(-5), "albums")
+
+    const usersData = await fetchData(`${API_URL}/users?_embed=posts&_embed=albums`)
+    let users = createHomeData(usersData.slice(-5), "users")
+
+
+    home.append(posts, albums, users)
 }
-function createHomeData(data) {
-    let postsTitle = document.createElement("h1")
-    let postsDiv = createHtmlElement("div", "posts-content")
-    postsDiv.append(postsTitle)
-    postsTitle.innerHTML = `Last 5 Posts`
-
+function createHomeData(data, type) {
+    let title = document.createElement("h1")
+    let div = createHtmlElement("div", `${type}-wrap`)
+    div.append(title)
+    title.innerHTML = `Last 5 ${type}`
+    let content
     data.forEach(element => {
-        let postDiv = createHtmlElement("div", "post-class")
-        let postTitle = document.createElement("h2")
-        let postAuthor = document.createElement("div")
-        let postContent = document.createElement("p")
+        if (type === "posts") {
+            content = firstLetterUpperCase(element.body)
+        } else if (type === "albums") {
+            let photoLink = document.createElement("a")
+            let albumPicture = document.createElement("img")
+            let photosDiv = createHtmlElement("div", "photos")
+            let photosTitle = createHtmlElement("h3", "photos-title")
 
-        let allUserPosts = document.createElement("span")
+            photosTitle = firstLetterUpperCase(element.photos[0].title)
+            photoLink.href = `./album.html?id=${element.id}`
+            albumPicture.src = element.photos[0].thumbnailUrl
+            photoLink.append(albumPicture)
+            photosDiv.append(photosTitle, photoLink)
 
-        postTitle.textContent = firstLetterUpperCase(element.title)
-        postAuthor.innerHTML = `<a href="./user.html?id=${element.userId}">Author: ${element.user.name}</a>`
-        postContent.textContent = firstLetterUpperCase(element.body)
-        allUserPosts.innerHTML = `<a href="./posts.html?id=${element.userId}">Other posts by ${element.user.name}</a>`
+            content = photosDiv.innerHTML
+        }
+        let elementDiv = createHtmlElement("div", `${type}-content`)
+        let elementTitle = document.createElement("h2")
+        let elementAuthor = document.createElement("div")
+        let elementContent = document.createElement("p")
+        let allUserElements = document.createElement("span")
 
-        postDiv.append(postTitle, postAuthor, postContent, allUserPosts)
-        postsDiv.append(postDiv)
+        if (type === "users") {
+            elementTitle.textContent = firstLetterUpperCase(element.name)
+            elementContent.innerHTML = `Nick: ${element.username} is from ${element.address.city} city. Has ${element.posts.length} posts and ${element.albums.length} albums`
+            allUserElements.innerHTML = `<a href="./user.html?id=${element.id}">Other about ${element.name}</a>`
+
+        } else {
+            elementTitle.textContent = firstLetterUpperCase(element.title)
+            elementAuthor.innerHTML = `<a href="./user.html?id=${element.userId}">Author: ${element.user.name}</a>`
+            elementContent.innerHTML = content
+            allUserElements.innerHTML = `<a href="./${type}.html?id=${element.userId}">Other ${type} by ${element.user.name}</a>`
+        }
+
+        elementDiv.append(elementTitle, elementAuthor, elementContent, allUserElements)
+        div.append(elementDiv)
     });
 
-    return postsDiv
+    return div
 }
 // 10. Sukurti pagrindinį puslapį (index.html). Jame laisva forma turi būti atvaizduoti:
 //   1.1. Įrašai (post).
